6c7af4eff02bab683cccb21ca3fdb0de9b64fa35
Hannah M.M. Eriksen <hannah_eriksen@hotmail.com>
2021-04-01 19:15:48 +0200

    Initial commit

---
+++ src/main/java/oblig_2_datsky/packages/Oblig2DatSkyApplication.java
@@ -0,0 +1,13 @@
+package oblig_2_datsky.packages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig2DatSkyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oblig2DatSkyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/oblig_2_datsky/packages/Oblig2DatSkyApplicationTests.java
@@ -0,0 +1,13 @@
+package oblig_2_datsky.packages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig2DatSkyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
