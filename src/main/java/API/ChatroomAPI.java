9c82690745e9d761994948db63f0a989e37c3229
Hannah M.M. Eriksen <hannah_eriksen@hotmail.com>
2021-04-06 23:13:22 +0200

    Legger inn en liten todo

--- src/main/java/API/ChatroomAPI.java
+++ src/main/java/API/ChatroomAPI.java
@@ -12,6 +12,9 @@ import java.util.ArrayList;
 @RestController
 public class ChatroomAPI {
 
+
+    //TODO: 1) Endre GetMapping til PostMapping
+    //todo: 2) Legge inn hvem som har opprettet chatrommet med brukerID 
     @GetMapping("/addOne")
     public void addOne(String roomName){
         Chatroom newChatroom = new Chatroom(roomName);
