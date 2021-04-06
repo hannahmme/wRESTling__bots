31bab43dd123d7f5d763885b6a85a6acc1ce81e5
Hannah M.M. Eriksen <hannah_eriksen@hotmail.com>
2021-04-06 20:28:05 +0200

    Sjekker at det funker å kalle på getAll-APImetode (chatroom)

--- src/main/java/API/ChatroomAPI.java
+++ src/main/java/API/ChatroomAPI.java
@@ -1,10 +1,24 @@
 package API;
 
+import Objects.Chatroom;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ChatroomAPI {
-    
+
+    @GetMapping("/getAll")
+    public String getAll(){
+        return "Hello from getAllChatrooms";
+    }
+
+    @PostMapping("/deleteChatroom")
+    public void deleteChatroom(){
+    }
+
+
 
 
 
